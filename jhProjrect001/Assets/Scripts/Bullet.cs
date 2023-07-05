67df5ed0a408b6f1658b7b5d2b96f87238e2a088
ZEZE\a0104 <a01049469494@gmail.com>
2023-07-05 17:54:36 +0900

    feat : dodgegame complete

--- jhProjrect001/Assets/Scripts/Bullet.cs
+++ jhProjrect001/Assets/Scripts/Bullet.cs
@@ -30,5 +30,6 @@ public class Bullet : MonoBehaviour
             }
         }
     }
+    
 
 }
--- jhProjrect001/Assets/Scripts/PlayerControler.cs
+++ jhProjrect001/Assets/Scripts/PlayerControler.cs
@@ -46,5 +46,8 @@ public class PlayerControler : MonoBehaviour
     public void Die()
     {
         gameObject.SetActive(false);
+
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        gameManager.EndGame();
     }
 }//class PlayerControler
